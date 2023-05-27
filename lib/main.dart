00f5359116366423319a51bd2186eb6c154473a6
BuildTools <unconfigured@null.spigotmc.org>
2023-05-28 01:54:42 +0200

    change qrcode to maps.seekode.dev from big map url. I also implemented an functionality for limit windows resize (one windows os only for now)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app_themes.dart';
 import 'providers/language/language.dart';
 import 'providers/theme/theme_state.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  if (!kIsWeb && Platform.isWindows) {
+    WidgetsFlutterBinding.ensureInitialized();
+    // Must add this line.
+    await windowManager.ensureInitialized();
+    WindowOptions windowOptions = const WindowOptions(
+      minimumSize: Size(350, 500),
+      center: true,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
   runApp(const ProviderScope(child: MyApp()));
 }
 
